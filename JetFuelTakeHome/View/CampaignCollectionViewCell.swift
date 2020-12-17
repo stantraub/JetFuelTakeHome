c3cd794715e9d85de8f5f8567ecf15e5039ee0cb
Stanley <stanley.traub@gmail.com>
2020-12-16 17:01:07 -0800

    Set up media collection view

--- JetFuelTakeHome/View/CampaignCollectionViewCell.swift
+++ JetFuelTakeHome/View/CampaignCollectionViewCell.swift
@@ -12,11 +12,13 @@ class CampaignCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    static let identifier = "CampaignCollectionViewCell"
+    
     var viewModel: CampaignCellViewModel? {
         didSet { configureWithViewModel() }
     }
     
-    static let identifier = "CampaignCollectionViewCell"
+    private var medias = [Media]()
     
     private let headerContainerView: UIView = {
         let view = UIView()
@@ -26,8 +28,8 @@ class CampaignCollectionViewCell: UICollectionViewCell {
     
     private let dividerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .secondarySystemBackground
-        view.setHeight(1)
+        view.backgroundColor = #colorLiteral(red: 0.8757564425, green: 0.9350169301, blue: 0.9470222592, alpha: 1)
+        view.setHeight(2)
         return view
     }()
     
@@ -52,23 +54,30 @@ class CampaignCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
-    private let collectionView: UICollectionView
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0,
+                                           left: 20,
+                                           bottom: 0,
+                                           right: 0)
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
+        cv.showsVerticalScrollIndicator = false
+        cv.backgroundColor = .systemGroupedBackground
+        return cv
+    }()
     
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: frame.width / 3, height: frame.height / 2)
-        
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-
-        
-        
         super.init(frame: frame)
-
         configureUI()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CampaignMediaCollectionViewCell.self, forCellWithReuseIdentifier: CampaignMediaCollectionViewCell.identifier)
+
         
     }
     
@@ -79,8 +88,6 @@ class CampaignCollectionViewCell: UICollectionViewCell {
     // MARK: - Helpers
     
     private func configureUI() {
-        backgroundColor = .systemGroupedBackground
-        
         addSubview(headerContainerView)
         headerContainerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor)
         headerContainerView.setHeight(120)
@@ -99,6 +106,9 @@ class CampaignCollectionViewCell: UICollectionViewCell {
         
         addSubview(dividerView)
         dividerView.anchor(top: headerContainerView.bottomAnchor, left: leftAnchor, right: rightAnchor)
+        
+        addSubview(collectionView)
+        collectionView.anchor(top: dividerView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
     }
 
     
@@ -107,5 +117,41 @@ class CampaignCollectionViewCell: UICollectionViewCell {
         campaignIcon.sd_setImage(with: viewModel.campaignIconURL)
         campaignNameLabel.text = viewModel.campaignName
         payPerInstallLabel.attributedText = viewModel.payPerInstall
+    }
+}
+
+extension CampaignCollectionViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        guard let viewModel = viewModel else { return 0 }
+//        return viewModel.campaign.medias.count
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let media = medias[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CampaignMediaCollectionViewCell.identifier, for: indexPath) as! CampaignMediaCollectionViewCell
+        return cell
+    }
+    
+    
+}
+
+extension CampaignCollectionViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = collectionView.frame.height * 0.85
+        let width = frame.width / 3.7
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("sup")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
--- JetFuelTakeHome/View/CampaignMediaCollectionViewCell.swift
+++ JetFuelTakeHome/View/CampaignMediaCollectionViewCell.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
-class CampaignMediaCollectionViewCell: UIView {
+class CampaignMediaCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    static let identifier = "CampaignMediaCollectionViewCell"
+    
     // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Helpers
 }
